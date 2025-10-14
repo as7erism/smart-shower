c09b7b1db5b8568022e6ba251d80247600f07a47
muhlenlogan8 <muhlenlogan8@gmail.com>
2025-10-13 21:25:15 -0400

    Added tailwindcss

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-center text-4xl text-blue-800">Test page 2</h1>
+<div class="flex justify-center mt-4">
+    <a href="/" class="bg-green-600 text-white px-4 py-2 rounded inline-block hover:bg-green-500">
+        Go to main Page
+    </a>
+</div>
