5cd9fae9cbe964afbf1c4d4b48ae13b2fb763e15
aster <astermayhew@proton.me>
2025-10-15 16:49:47 -0400

    change formatting, state management (#2)

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { getState } from '$lib/state.svelte';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    let state = getState();
+    console.log(`state ${JSON.stringify(state)}`);
+  });
+</script>
+
+<h1>hi</h1>
