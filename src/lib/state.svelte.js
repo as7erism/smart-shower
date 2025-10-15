eb5ed759ea260aec254153e3b56277a9139bbf63
aster <astermayhew@proton.me>
2025-10-15 16:35:45 -0400

    change formatting, state management

---
+++ src/lib/state.svelte.js
@@ -0,0 +1,56 @@
+let state = $state(undefined);
+
+export function getState() {
+  if (state === undefined) {
+    state = JSON.parse(localStorage.getItem('state'));
+    if (state === null) {
+      saveState({
+        profiles: {
+          Aster: {
+            presets: {
+              default: {
+                temperature: 100,
+                pressure: 60
+              },
+              cold: {
+                temperature: 80,
+                pressure: 60
+              }
+            }
+          },
+          Kaleb: {
+            presets: {
+              default: {
+                temperature: 100,
+                pressure: 60
+              }
+            }
+          },
+          Divyesh: {
+            presets: {
+              default: {
+                temperature: 100,
+                pressure: 60
+              }
+            }
+          },
+          Logan: {
+            presets: {
+              default: {
+                temperature: 100,
+                pressure: 60
+              }
+            }
+          }
+        }
+      });
+    }
+  }
+
+  return state;
+}
+
+export function saveState(data) {
+  state = data;
+  localStorage.setItem('state', JSON.stringify(state));
+}
