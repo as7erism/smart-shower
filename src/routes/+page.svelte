45ea1a25ff5541ec505353ceb1ac1e622c3ac375
Logan Muhlen <100247149+muhlenlogan8@users.noreply.github.com>
2025-10-15 02:33:03 -0400

    Added tailwindcss (#1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-center text-4xl text-blue-800">Test page</h1>
+<div class="flex justify-center mt-4">
+    <a href="/test" class="bg-green-600 text-white px-4 py-2 rounded inline-block hover:bg-green-500">
+        Go to other test Page
+    </a>
+</div>
+
