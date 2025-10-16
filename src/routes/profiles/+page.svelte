c84c059531e711fd395fc1579c185a658950bf5e
aster <astermayhew@proton.me>
2025-10-16 18:55:22 -0400

    proof of concept

---
+++ src/routes/profiles/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getState } from '$lib/state.svelte';
+  import { onMount } from 'svelte';
+
+  let profiles = $state({});
+
+  onMount(() => {
+    profiles = getState().profiles;
+  });
+</script>
+
+{#each Object.entries(profiles) as [name, value]}
+  <h1>{name}:</h1>
+  <p>temperature: {value.temperature}</p>
+  <p>pressure: {value.pressure}</p>
+{/each}
