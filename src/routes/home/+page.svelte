bf9c0a86a9d46e2033f7b8db63666ba53d0d7bd8
divyeshbommana <divyeshbommana@gmail.com>
2025-10-16 12:57:28 -0400

    added home page along with therm component

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { getState } from '$lib/state.svelte';
+  import { onMount } from 'svelte';
+  import Termometer from './termometer.svelte';
+
+  onMount(() => {
+    let state = getState();
+    console.log(`state ${JSON.stringify(state)}`);
+  });
+</script>
+
+<h1>This is the home page</h1>
+<Termometer />
