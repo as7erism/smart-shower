89828884ab135aa9a2b3a14eac0ff782c79a2d4b
aster <astermayhew@proton.me>
2025-10-17 02:22:41 -0400

    "functional" status page, unquote obj keys

--- src/routes/profiles/+page.svelte
+++ src/routes/profiles/+page.svelte
@@ -1,16 +1,62 @@
 <script>
-  import { getState } from '$lib/state.svelte';
+  import { getState, saveState } from '$lib/state.svelte';
   import { onMount } from 'svelte';
+  import Profile from './profile.svelte';
 
-  let profiles = $state({});
+  let profiles = $state([]);
 
   onMount(() => {
-    profiles = getState().profiles;
+    profiles = Object.entries(getState().profiles).reduce((acc, [name, value]) => {
+      acc.push({
+        name: name,
+        temperature: value.temperature,
+        pressure: value.pressure
+      });
+      return acc;
+    }, []);
   });
+
+  function nameChangeCallbackFactory(index) {
+    return (newName) => {
+      for (const {name} of profiles) {
+        if (name == newName) {
+          window.alert("BAD DOG");
+          return false;
+        }
+      }
+
+      profiles[index].name = newName;
+      return true;
+    }
+  }
+
+  function saveProfiles() {
+    let state = getState();
+    let profilesObject = {};
+    for (const profile of profiles) {
+      profilesObject[profile.name] = {};
+      profilesObject[profile.name]["temperature"] = profile.temperature;
+      profilesObject[profile.name]["pressure"] = profile.pressure;
+    }
+    state.profiles = profilesObject;
+    console.log(`saving ${JSON.stringify(state)}`);
+    saveState(state);
+  }
+
 </script>
 
-{#each Object.entries(profiles) as [name, value]}
-  <h1>{name}:</h1>
-  <p>temperature: {value.temperature}</p>
-  <p>pressure: {value.pressure}</p>
+{#each profiles as _, i}
+  <Profile
+    name={profiles[i].name}
+    bind:temperature={profiles[i].temperature}
+    bind:pressure={profiles[i].pressure}
+    onNameChange={nameChangeCallbackFactory(i)}
+  />
 {/each}
+
+
+<button onclick={() => profiles.push({name: `${Date.now()}`, temperature: 100, pressure: 60})}>add</button>
+<br>
+<button onclick={() => window.alert(JSON.stringify(profiles))}>check the thing</button>
+<br>
+<button onclick={saveProfiles}>save</button>
